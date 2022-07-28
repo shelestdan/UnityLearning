5e94643d9c1d59eeb0789691173d0f59da71da1c
shelestdan <daniil.shelesteev@yandex.ru>
2022-07-28 21:41:17 +0300

    d01

---
+++ d00/Assets/ex01/Scripts/Note.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private Vector3 startetPosition;
+    [SerializeField] private KeyCode keyCode;
+    private float _speed;
+
+    void Start()
+    {
+        _speed = Random.Range(minSpeed, maxSpeed);
+        transform.position = startetPosition;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(keyCode))
+        {
+            Debug.Log("Precision: " + (transform.position.y - -4.46f));
+            Destroy(gameObject);
+        }
+        transform.position += Vector3.down * _speed * Time.deltaTime;
+    }
+
+    private void OnBecameInvisible() //вылет объекта за камеру
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ d00/Assets/ex01/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] objects; // создание массива 
+    private float time = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if(time > 2.8f)
+        {
+            Instantiate(objects[Random.Range(0, objects.Length)]); // создание объекта на сцене
+            time = 0f;
+        }
+
+    }
+}
